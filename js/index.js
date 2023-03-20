065141e8f3ede5bf935c3b262eb5a1cc017d1d56
BeYoddy <changwoon4@naver.com>
2023-03-21 01:57:48 +0900

    fade-in

--- js/index.js
+++ js/index.js
@@ -30,3 +30,11 @@ window.addEventListener("scroll", function () {
     });
   }
 });
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
