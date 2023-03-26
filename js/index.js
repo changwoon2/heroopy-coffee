93f82802e3c28313c909b3918e3a87d10e7e81f0
BeYoddy <changwoon4@naver.com>
2023-03-26 14:48:39 +0900

    youtube

--- js/index.js
+++ js/index.js
@@ -70,3 +70,25 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.add("hide");
   }
 });
+
+gsap.to(".floating1", 1.5, {
+  delay: 1,
+  y: 15,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut,
+});
+gsap.to(".floating2", 2, {
+  delay: .5,
+  y: 15,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut,
+});
+gsap.to(".floating3", 3, {
+  delay: 1.5,
+  y: 20,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut,
+});
